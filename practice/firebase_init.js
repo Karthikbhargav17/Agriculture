a6e04ed296f822c8e2b5cade3c86af09503cf3f3
Karthik Y <73058106+Karthikbhargav17@users.noreply.github.com>
2020-12-20 10:55:18 +0530

    Add files via upload

---
+++ practice/firebase_init.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCcfNZRZEIugsIWSP17nj4inO8jh-GEHG8",
+    authDomain: "agriculture-85bbe.firebaseapp.com",
+    projectId: "agriculture-85bbe",
+    storageBucket: "agriculture-85bbe.appspot.com",
+    messagingSenderId: "1097947396822",
+    appId: "1:1097947396822:web:d67343eebba0af3d91a777",
+    measurementId: "G-YJV0NKP44J"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+const firebaseRealtimeDatabase = firebase.database();
